4efe31bc9a58f7beedec2527f9fb7b15bd87536a
syllabuspdf <99137616+syllabuspdf@users.noreply.github.com>
2023-12-24 22:02:44 +0530

    Update header.php

--- wp/wp-content/themes/blogita/header.php
+++ wp/wp-content/themes/blogita/header.php
@@ -110,13 +110,12 @@
 		</div>
 	</div>
 
-	 <script>
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	 <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TG7NN60NDW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
- ga('create', 'G-TG7NN60NDW', 'auto');
- ga('send', 'pageview');
-
+  gtag('config', 'G-TG7NN60NDW');
 </script>
