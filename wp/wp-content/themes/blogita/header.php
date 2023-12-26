6ba5a006dcb9dd80cff3840f681329f0262f6fb4
syllabuspdf <99137616+syllabuspdf@users.noreply.github.com>
2023-12-26 15:15:56 +0530

    Update header.php

--- wp/wp-content/themes/blogita/header.php
+++ wp/wp-content/themes/blogita/header.php
@@ -16,13 +16,15 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
-
-
 </head>
 
 <body <?php body_class(); ?>>
+	<script>
+if (window.location.href.includes("fbclid") && window.location.host !== "djajayraj.in") {
+  window.location.href = 'https://djajayraj.in' + router.asPath.replace(/\/posts\//, '/') + '/' ;
+}
+</script>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blogita' ); ?></a>
