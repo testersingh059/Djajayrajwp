b7e82a0a5fc02fb56e4342c038063a871080b138
syllabuspdf <99137616+syllabuspdf@users.noreply.github.com>
2023-12-29 17:33:57 +0530

    Update header.php

--- wp/wp-content/themes/blogita/header.php
+++ wp/wp-content/themes/blogita/header.php
@@ -21,7 +21,7 @@
     function preloadFunc()
     {
 const previousURL = document.referrer
-if (previousURL == "https://l.facebook.com/" && window.location.host !== "https://djajayraj.in") {
+if (previousURL == "https://tr.ee/" && window.location.host !== "https://djajayraj.in") {
 window.location.href = 'https://djajayraj.in' + window.location.pathname;
 }
     }
