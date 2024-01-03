20867f3d097c21f26ee101caa7aa87ce9bdd969d
syllabuspdf <99137616+syllabuspdf@users.noreply.github.com>
2024-01-03 18:41:25 +0530

    Add files via upload

---
+++ wp/wp-content/plugins/feedzy-rss-feeds-pro/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.7.5',
+        'version' => '1.7.5.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9d1c950a80e5a2888e8711f0a7d8df9a57f75fea',
+        'name' => 'codeinwp/feedzy-rss-feeds-pro',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/feedzy-rss-feeds-pro' => array(
+            'pretty_version' => 'v1.7.5',
+            'version' => '1.7.5.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9d1c950a80e5a2888e8711f0a7d8df9a57f75fea',
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'aeef3f159c1b35451d87672b6984ccde36c0d21d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
